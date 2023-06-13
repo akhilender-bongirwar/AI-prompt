3790077333dd69867467c0377ef386e64a0521e5
Akhil <akhilenderb9@gmail.com>
2023-06-13 22:03:20 +0530

    Added the create post functionality and it renders on the Home page

---
+++ Users/Akhil/Desktop/promtify/app/profile/page.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import Profile from '@components/Profile'
+
+const Profile = () => {
+  return (
+    <Profile
+     name="My"
+     
+    />  
+  )
+}
+
+export default Profile
--- Users/Akhil/Desktop/promtify/components/Feed.jsx
+++ Users/Akhil/Desktop/promtify/components/Feed.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
+import PromptCard from './PromptCard';
+
+
+const PromptCardList = ({data, handleTagClick})=>{
+  // console.log(data);
+    return(
+        <div className='mt-16 prompt_layout'>
+        {data.map((post)=>(<PromptCard key={post._id} post={post} handleTagClick={handleTagClick}/>
+        ))}
+      </div>  
+    )
+}
+
 
 const Feed = () => {
+  const [searchTxt, setSearchTxt] = useState('');
+  const [allPost, setAllPost] = useState([]);
+  const handleChange= ()=>{
+    
+  }
+  const fetchPosts = async () =>{
+    const res = await fetch('/api/prompt');
+    const data = await res.json();
+    setAllPost(data);
+  }
+
+  useEffect(()=>{
+    fetchPosts();
+  },[]);
+
   return (
-    <div>Feed</div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input 
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchTxt}
+          onChange={handleChange}
+          required
+          className='search_input peer'
+        />
+      </form>
+      <PromptCardList
+        data={allPost}
+        handleTagClick={()=>{}}
+      />
+      
+    </section>
   )
 }
 
