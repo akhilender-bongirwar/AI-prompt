0fad65858af961f0b772bb620ad4297e6ae74851
Akhil <akhilenderb9@gmail.com>
2023-06-13 23:42:04 +0530

    Made all the necessary routes for edit and delete end point

--- Users/Akhil/Desktop/promtify/app/profile/page.jsx
+++ Users/Akhil/Desktop/promtify/app/profile/page.jsx
@@ -1,14 +1,41 @@
 "use client"
 
 import Profile from '@components/Profile'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
-const Profile = () => {
+const MyProfile = () => {
+    const router = useRouter();
+    const {data:session} = useSession();
+    const [post, setAllPost] = useState([]);
+
+    const fetchPosts = async () =>{
+        const res = await fetch(`/api/users/${session?.user.id}/posts`);
+        const data = await res.json();
+        setAllPost(data);
+      }
+    
+      useEffect(()=>{
+        if(session?.user.id) fetchPosts();
+      },[]);
+    
+    const handleEdit = (post) =>{
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+    const handleDelete = async (post)=>{
+        
+    }
+
   return (
     <Profile
      name="My"
-     
+     desc="Welcome to My profile page"
+     data={post}
+     handleEdit={handleEdit}
+     handleDelete={handleDelete}
     />  
   )
 }
 
-export default Profile
+export default MyProfile
